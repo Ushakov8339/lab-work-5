d4bafe2f66690a266b95a80facdd3596ff810d4d
Sergey <ushakov-01@mail.ru>
2025-11-24 06:15:31 +0300

    add method findMaxInArray in dev

--- Main.java
+++ Main.java
@@ -1,5 +1,15 @@
 class Main{
     public static void main(String[] args) {
         System.out.println("Это добавлено в ветке dev");
+        findMaxInArray(new int[]{3, 7, 2, 9, 1});
+    }
+    public static void findMaxInArray(int[] array) {
+        int max = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
+            }
+        }
+        System.out.print("Максимальный элемент в массиве: " + max);
     }
 }
