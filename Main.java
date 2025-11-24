f7b87c487a89a340ea88cf76d5cc520ae136bc71
Sergey <ushakov-01@mail.ru>
2025-11-24 05:40:12 +0300

    add methods call

--- Main.java
+++ Main.java
@@ -2,6 +2,8 @@ class Main{
     public static void main(String[] args) {
         System.out.println("Это добавлено в ветке main");
         calculateSum(2,2);
+        printNumbers(4);
+        checkEvenOdd(6);
     }
     public static void calculateSum(int a, int b) {
         int result = a + b;
