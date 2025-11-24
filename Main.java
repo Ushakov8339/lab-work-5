fabc5972994e8dbff626743cb60c9f5323ac9d29
Sergey <ushakov-01@mail.ru>
2025-11-24 05:34:08 +0300

    add method calculateSum

--- Main.java
+++ Main.java
@@ -1,5 +1,10 @@
 class Main{
     public static void main(String[] args) {
         System.out.println("Это добавлено в ветке main");
+        calculateSum(2,2);
+    }
+    public static void calculateSum(int a, int b) {
+        int result = a + b;
+        System.out.print("Сумма: " + result);
     }
 }
